f71b6452010e6c2e4569354d17d5331ddc787a4f
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-07-27 15:11:05 -0400

    Add playRound function

--- rps.js
+++ rps.js
@@ -10,5 +10,39 @@ function getPlayerChoice() {
     while (!validChoices.includes(choice)) {
         choice = prompt("Enter rock, paper, or scissors: ").toLowerCase();
     }
+
+    return choice;
 }
 
+function getWinner(playerChoice, computerChoice) {
+    if ((playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock") ||
+        (playerChoice == "scissors" && computerChoice == "paper")) {
+            return 1;
+    } else if((computerChoice == "rock" && playerChoice == "scissors") ||
+            (computerChoice == "paper" && playerChoice == "rock") ||
+            (computerChoice == "scissors" && playerChoice == "paper")) {
+                return 2;
+    } else {
+        return 0;
+    }
+}
+
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    let winner = getWinner(playerChoice, computerChoice);
+    switch (winner) {
+        case 0: return "Tie";
+        case 1: return `You Win! ${capitalize(playerChoice)} beats ${capitalize(computerChoice)}`;
+        case 2: return `You Lose! ${capitalize(computerChoice)} beats ${capitalize(playerChoice)}`;
+    }
+}
+
+function capitalize(word) {
+    let firstLetter = word.substring(0,1);
+    let restOfWord = word.substring(1);
+    return firstLetter.toUpperCase() + restOfWord.toLowerCase();
+}
+
+console.log(playRound());
